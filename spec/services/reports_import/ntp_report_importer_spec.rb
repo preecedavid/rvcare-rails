931b99f3afb969d13f75318c3e4ca3de97985252
preecedavid <davidpreece.upwork@gmail.com>
2021-04-17 02:31:51 +0300

    add specs for service objects

---
+++ spec/services/reports_import/ntp_report_importer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module ReportsImport
+  RSpec.describe NtpReportImporter do
+    context 'ntp reports' do
+      it 'returns operation logs'
+    end
+  end
+end
