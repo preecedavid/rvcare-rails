63e424b3d55eda8ff319d437c44d603d1895cc42
preecedavid <davidpreece.upwork@gmail.com>
2021-04-17 16:36:36 +0300

    ntp report importer minimal logic

--- app/services/reports_import/ntp_report_importer.rb
+++ app/services/reports_import/ntp_report_importer.rb
@@ -1,13 +1,29 @@
 module ReportsImport
   class NtpReportImporter
-    def initialize(_data); end
+    def initialize(partner: , data:)
+      @partner = partner
+      @data = data
+    end
 
-    def call; end
+    def call
+      @data.each do |line|
+        Sales.create(
+          partner_report_id: partner_report.id,
+          dealer_id: line[:ntp_account],
+          reported_on: line[:reported_on],
+          value: line[:amount]
+        )
+      end
+    end
 
     def logs
       ["logs placeholder"]
     end
 
     private
+
+    def partner_report
+      @partner_report ||= @partner.current_report
+    end
   end
 end
--- spec/services/reports_import/ntp_report_importer_spec.rb
+++ spec/services/reports_import/ntp_report_importer_spec.rb
@@ -4,7 +4,42 @@ require 'rails_helper'
 
 module ReportsImport
   RSpec.describe NtpReportImporter do
+    let(:year) { Date.today.year }
+
     context 'ntp reports' do
+      let!(:partner_report) do
+        FactoryBot.create(:partner_report, year: year)
+      end
+
+      describe '#call' do
+        let(:dealer) { FactoryBot.create(:dealer) }
+        let(:input_data) do
+          [
+            { ntp_account: dealer.id, amount: 10, reported_on: "#{year}-04-10" },
+            { ntp_account: dealer.id, amount: 17, reported_on: "#{year}-04-11" },
+          ]
+        end
+
+        context 'valid input data' do
+          subject { NtpReportImporter.new(data: input_data, partner: partner_report.partner).call }
+
+          it 'adds records to entries table' do
+            expect { subject }.to change(Entry, 'count').by(input_data.size)
+          end
+
+          it 'saves the correct data', :aggregate_failures do
+            subject
+            entries = Entry.last(2)
+
+            expect(entries.map(&:type)).to all(eq("Sales"))
+            expect(entries.map(&:partner_report_id)).to all(eq(partner_report.id))
+            expect(entries.map(&:dealer_id)).to all(eq(dealer.id))
+            expect(entries.map(&:reported_on)).to contain_exactly(Date.parse("2021-04-10"), Date.parse("2021-04-11"))
+            expect(entries.map(&:value)).to contain_exactly(10, 17)
+          end
+        end
+      end
+
       it 'returns operation logs'
     end
   end
